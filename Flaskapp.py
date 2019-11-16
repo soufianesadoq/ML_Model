78cc70b588ca5b6e2680da2319147aa8c6622f6b
no84hit <soufiane.sadoq@uni-jena.de>
2019-11-16 16:46:59 +0100

    version 2

--- Flaskapp.py
+++ Flaskapp.py
@@ -1,18 +1,19 @@
 import pandas as pd
+import numpy as np
 from flask import Flask, jsonify, request, render_template
 import pickle
 
 # load model
-model1 = pickle.load(open('knn_gewicht.pkl','rb'))
-model2 = pickle.load(open('knn_uniform.pkl','rb'))
+model1 = pickle.load(open('knn_gewicht.pkl', 'rb'))
+model2 = pickle.load(open('knn_uniform.pkl', 'rb'))
 
 # app
-app = Flask(__name__,template_folder='Template')
+app = Flask(__name__, template_folder='Template')
 
 # routes
 @app.route('/')
 def index():
-    return render_template('home.html')
+    return render_template('home.html',methods=['POST'])
 ##@app.route('/home', methods=['GET','POST'])
 #def predict():
     # get data
@@ -45,5 +46,28 @@ def index():
         #print(pred)
         #return render_template('index.html', sentiment=pred['airline_sentiment_predictions'][0])
     #return render_template('index.html', sentiment='')
+@app.route('/prediction',methods=['POST'])
+def prediction():
+    if request.method == 'POST':
+
+        Temperatur = request.form['Temperatur']
+        relativeLuftfeuchtigkeit = request.form['relativeLuftfeuchtigkeit']
+        Niederschlag = request.form['Niederschlag']
+        Schnee = request.form['Schnee']
+        model = request.form['model']
+        data = {'Temperatur': [Temperatur], 'relative Luftfeuchtigkeit': [relativeLuftfeuchtigkeit], 'Niederschlag': [Niederschlag], 'Schnee': [Schnee]}
+        df = pd.DataFrame(data)
+        print(model)
+        print(pd.DataFrame(data))
+        if model == 1:
+            pred = model1.predict(df)
+            print(pred)
+            return render_template('prediction.html' , result = pred , model = 'gewichtes Model')
+
+        if model == 2:
+            pred = model2.predict(df)
+            print(pred)
+            return render_template('prediction.html' , result = pred , model = 'Uniform Model')
+    return render_template('home.html')
 if __name__ == '__main__':
     app.run(port = 800, debug=True)
