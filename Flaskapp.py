898d0490338159a222cdfea8618548cadca86a06
no84hit <soufiane.sadoq@uni-jena.de>
2019-11-16 14:32:10 +0100

    make

--- Flaskapp.py
+++ Flaskapp.py
@@ -1,33 +1,49 @@
 import pandas as pd
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 import pickle
 
 # load model
-model = pickle.load(open('model.pkl','rb'))
+model1 = pickle.load(open('knn_gewicht.pkl','rb'))
+model2 = pickle.load(open('knn_uniform.pkl','rb'))
 
 # app
-app = Flask(__name__)
+app = Flask(__name__,template_folder='Template')
 
 # routes
-@app.route('/', methods=['POST'])
-
-def predict():
+@app.route('/')
+def index():
+    return render_template('home.html')
+##@app.route('/home', methods=['GET','POST'])
+#def predict():
     # get data
-    data = request.get_json(force=True)
-
+ #   data = request.get_json(force=True)
+  #  if request.method == 'POST':
+   #     data = request.form.get(data)
+    #    data_df = pd.DataFrame(data)
+     #   erg = model1.predict(data_df)
+      #  print(erg)
+       # return render_template('home.html', perediction = erg[0])
     # convert data into dataframe
-    data.update((x, [y]) for x, y in data.items())
-    data_df = pd.DataFrame.from_dict(data)
+    #data.update((x, [y]) for x, y in data.items())
+    #data_df = pd.DataFrame.from_dict(data)
 
     # predictions
-    result = model.predict(data_df)
+    #result = model.predict(data_df)
 
     # send back to browser
-    output = {'results': int(result[0])}
+    #print(result)
+    #return render_template('index.html', sentiment=result['airline_sentiment_predictions'][0])
 
     # return data
-    return jsonify(results=output)
-
+    #return jsonify(results=output)
+    #if request.method == 'POST':
+     #   data = request.form.get('text')
+        # Make prediction
+      #  df = pd.DataFrame([str(data)], columns=['content'])
+       # print(df.head())
+        #pred = model.predict(data_df=df)
+        #print(pred)
+        #return render_template('index.html', sentiment=pred['airline_sentiment_predictions'][0])
+    #return render_template('index.html', sentiment='')
 if __name__ == '__main__':
-    app.run(port = 5000, debug=True)
-
+    app.run(port = 800, debug=True)
